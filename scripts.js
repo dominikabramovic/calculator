33c4e91dbc33e65ec34ad545ee575917bdd1d3f7
Dominik <dominik.7@live.com>
2023-09-22 11:13:51 +0200

    Create basic UI

---
+++ scripts.js
@@ -0,0 +1,7 @@
+function sum (a,b) {return a + b};
+function subtract (a,b) {return a - b};
+function multiply (a,b) {return a * b};
+function divide (a,b) {return a / b};
+
+let displayValue = 0;
+

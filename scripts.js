3a307e9560e176875f5542d8b6dc4e6902528985
Dominik <dominik.7@live.com>
2023-09-22 12:29:49 +0200

    Create logic for display, sum and erase functions
    Made the display reflect the current numbers being typed,
    assigned them to variables and created fully functioning sum function,
    along with erase and equals functions.
    
    Also implemented chaining operators.

--- scripts.js
+++ scripts.js
@@ -3,5 +3,54 @@ function subtract (a,b) {return a - b};
 function multiply (a,b) {return a * b};
 function divide (a,b) {return a / b};
 
-let displayValue = 0;
+let firstArgument = 0;
+let secondArgument = 0;
+let operation;
+
+//Handles number input and displays value
+let displayValue = '0';
+let display = document.querySelector('.display');
+let nums = document.querySelectorAll('.num');
+nums.forEach(num => num.addEventListener('click', () => {
+    displayValue === '0' ? displayValue = num.innerHTML : displayValue += num.innerHTML;
+    display.innerHTML = displayValue;
+}))
+
+let erase = document.querySelector('.erase');
+erase.addEventListener('click', () => {
+    displayValue = '0';
+    display.innerHTML = '0';
+    firstArgument = 0;
+    secondArgument = 0;
+})
+
+let sumOf = document.querySelector('.sum')
+sumOf.addEventListener('click', () => {
+    if (firstArgument === 0) {
+        firstArgument = parseInt(displayValue);
+        displayValue = '0';
+    }
+    else {
+        secondArgument = parseInt(displayValue);     
+        displayValue = sum (firstArgument, secondArgument);
+        display.innerHTML = displayValue;
+        firstArgument = parseInt(displayValue);
+        displayValue = '0';
+        secondArgument = 0;
+    }
+})
+
+let equals = document.querySelector('.equals');
+equals.addEventListener('click', () => {
+    if (firstArgument != 0) {
+        secondArgument = parseInt(displayValue);
+        displayValue = sum(firstArgument, secondArgument);
+        display.innerHTML = displayValue;
+        firstArgument = displayValue;
+        displayValue = '0';
+        secondArgument = 0;
+    }
+})
+
+
 
